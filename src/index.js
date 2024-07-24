b7f598a3bda9046552988d4e77d6be7fc2192b44
78068360+RichardGabelman@users.noreply.github.com <78068360+actuallychaz@users.noreply.github.com>
2024-07-24 05:20:24 -0700

    Update projectViewGenerate function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { Project } from "./modules/project.js";
 import { Todo } from "./modules/todo.js";
 import { ProjectManager } from "./modules/projectManager.js";
+import { HtmlGenerator } from "./modules/htmlGenerator.js";
 import './style.css';
 
 const manager = new ProjectManager();
@@ -9,5 +10,8 @@ const newTodo = new Todo("test", "this is a test description", "today", 1);
 study.addTodo(newTodo);
 manager.addProject(study);
 
-console.log(manager);
+const content = document.querySelector(".content");
+content.textContent = '';
+
+content.appendChild(HtmlGenerator.projectViewGenerate(manager));
 
--- src/modules/htmlGenerator.js
+++ src/modules/htmlGenerator.js
@@ -1,6 +1,6 @@
 import close from '../assets/close.svg';
 
-class HtmlGenerator {
+export class HtmlGenerator {
   static todoGenerate(todo) {
     const todoDiv = document.createElement("li");
     todoDiv.classList.add('todo');
@@ -18,9 +18,18 @@ class HtmlGenerator {
     checkTitle.appendChild(checkbox);
     checkTitle.appendChild(title);
 
+    const closeIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    closeIcon.setAttribute('viewBox', '0 0 24 24');
+
+    const svgTitle = document.createElementNS("http://www.w3.org/2000/svg", 'title');
+    svgTitle.textContent = "close";
+    closeIcon.appendChild(svgTitle);
+    const path = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+    path.setAttribute('d', "M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z");
+    closeIcon.appendChild(path);
 
     todoDiv.appendChild(checkTitle);
-    todoDiv.appendChild(close);
+    todoDiv.appendChild(closeIcon);
 
     return todoDiv;
   }
@@ -30,6 +39,7 @@ class HtmlGenerator {
     projectDiv.classList.add('project');
 
     const name = document.createElement("h2");
+    name.classList.add("name", "header");
     name.textContent = project.name;
 
     const todos = document.createElement("ul");
@@ -39,8 +49,13 @@ class HtmlGenerator {
       todos.appendChild(this.todoGenerate(project.todos[i]));
     }
 
+    const addTodo = document.createElement('btn');
+    addTodo.classList.add("addTodo", "footer");
+    addTodo.textContent = "+ todo +";
+
     projectDiv.appendChild(name);
     projectDiv.appendChild(todos);
+    projectDiv.appendChild(addTodo);
 
     return projectDiv;
   }
@@ -56,8 +71,17 @@ class HtmlGenerator {
       projects.appendChild(this.projectGenerate(projectManager.projects[i]));
     }
 
+    const controls = document.createElement("div");
+    controls.classList.add("controls");
+
+    const addProject = document.createElement("btn");
+    addProject.setAttribute('id', 'addProject');
+    addProject.textContent = '+';
+    controls.appendChild(addProject);
+
     projectView.appendChild(projects);
-    
+    projectView.appendChild(controls);
+
     return projectView;
   }
 }
