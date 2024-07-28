834486e3f26f9b7f089a25a39d3e4e057b626d07
78068360+RichardGabelman@users.noreply.github.com <78068360+actuallychaz@users.noreply.github.com>
2024-07-28 12:25:41 -0700

    Initial dist subtree commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  // TODO: Remove dev mode and tool when done
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: 'production',
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
